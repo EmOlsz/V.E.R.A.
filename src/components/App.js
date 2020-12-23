a34bbf92f810f633e67b4723ee676cbde0236e61
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2020-12-23 23:07:52 +0000

    finished animation, added static content

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Overlay from 'components/Overlay';
+import Welcome from 'components/Welcome';
 
 import 'styles/style.css';
 
 const App = () => {
   return (
     <>
-      <div>Hello Vera!</div>
       <Overlay />
+      <Welcome />
     </>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div className="welcome">
+            <div className="welcome-section">
+                <p className="welcome-paragraph">You must be wondering why I gathered you all here...</p>
+                <p className="welcome-paragraph">Let me intoduce you to your personal, fully adjustable application, which will hopefully be a handy tool during your new path into parenthood.</p>
+            </div>
+            <div className="welcome-section">
+                <h5 className="welcome-section-header">Uuummmm, this is a tasty burger!</h5>
+                <p className="welcome-paragraph">Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+            </div>
+            <div className="welcome-section">
+                <h5 className="welcome-section-header">Uuummmm, this is a tasty burger!</h5>
+                <p className="welcome-paragraph">Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+            </div>
+            <div className="welcome-section">
+                <h5 className="welcome-section-header">Uuummmm, this is a tasty burger!</h5>
+                <p className="welcome-paragraph">Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Welcome;
