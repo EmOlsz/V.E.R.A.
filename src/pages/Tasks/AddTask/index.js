3d7dee986a30a2a0bc11a974e8f413a418669e6d
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-01-10 22:49:36 +0000

    added quotes mode

--- src/pages/Tasks/AddTask/index.js
+++ src/pages/Tasks/AddTask/index.js
@@ -4,12 +4,7 @@ import { useDispatch } from 'react-redux';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 
 import { tasks } from 'reducers/tasks';
 
@@ -28,16 +23,6 @@ const AddTask = ({ isOpen, onModalClose }) => {
 
     return (
         <Dialog open={isOpen} onClose={onModalClose} fullScreen>
-            {/* <AppBar className="add-task-title-bar">
-                <Toolbar>
-                    <Typography className="add-task-title" variant="h6">
-                        New task
-                    </Typography>
-                    <IconButton edge="end" onClick={onModalClose}>
-                        <CloseIcon className="add-task-actions-element" />
-                    </IconButton>
-                </Toolbar>
-            </AppBar> */}
             <DialogContent className="add-task-content">
                 <textarea
                     className="add-task-input"
